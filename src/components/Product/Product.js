56e0c64376a2ea9f82f83bcd83bcc56682e1096d
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-02-26 10:31:58 +0600

    update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,21 +1,38 @@
-import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Product.css";
 
 const Product = (props) => {
-    // console.log(props.productAt.name);
-    console.log(props.productAt);
-    const {img, name, price} = props.productAt;
-    return (
-        <div className="product">
-            {/* <h1>This is product</h1> */}
-            <div>
-              <img src={props.productAt.img} alt=""/>
-            </div>
-            <div>
-            <h4><strong>Name: </strong>{name} <strong>Price:</strong>{price}$</h4>
-            </div>
-           
-        </div>
-    );
+  // console.log(props.productAt.name);
+//   console.log(props.productAt);
+    //  console.log(props);
+  const { img, name, price, seller, stock } = props.productAt;
+  return (
+    <div className="product">
+      {/* <h1>This is product</h1> */}
+      <div>
+        <img src={props.productAt.img} alt="" />
+      </div>
+      <div>
+        <h5 className="product-info">
+          <strong>Name: </strong>
+          {name}
+        </h5>
+        <br />
+        <p>
+          <small>by: {seller}</small>
+        </p>
+        <p>${price}</p>
+        <br />
+        <p>
+          <small>Only {stock} left in stock - Order soon.</small>
+        </p>
+        <button onClick={() => props.handleAddProduct(props.productAt)} className="main-button">add to cart</button> 
+        {/* <FontAwesomeIcon icon={faShoppingCart} /> */}
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,12 +8,16 @@ const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
 
+  const handleAddProduct = (productAt) => {
+        console.log('product added');
+    }
+
     return (
         <div className ="shop-container">
             <div className="product-container">
             
             {
-                products.map(product => <Product productAt={product}></Product>)
+                products.map(product => <Product handleAddProduct = {handleAddProduct} productAt={product}></Product>)
             }
             
          </div>
